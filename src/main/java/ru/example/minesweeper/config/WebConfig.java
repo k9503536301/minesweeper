7e051b48c2ed7cf1c7405b51c8d07928f8f72554
Kirill Shurygin <Kirill Shurygin>
2025-03-19 12:02:07 +0300

    add cors mapping

---
+++ src/main/java/ru/example/minesweeper/config/WebConfig.java
@@ -0,0 +1,19 @@
+package ru.example.minesweeper.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://minesweeper-test.studiotg.ru")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
